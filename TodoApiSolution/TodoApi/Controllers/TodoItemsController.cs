e2ed7ddcaec1a6b868aa02abba447bc8deae108f
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-01 22:00:52 +0900

    feat: 過剰な投稿を防止するを追加

--- TodoApiSolution/TodoApi/Controllers/TodoItemsController.cs
+++ TodoApiSolution/TodoApi/Controllers/TodoItemsController.cs
@@ -14,44 +14,65 @@ public class TodoItemsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
     {
-        return await _context.TodoItems.ToListAsync();
+        return await _context.TodoItems.Select(x => ItemToDTO(x)).ToListAsync();
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
     {
-        var item = await _context.TodoItems.FindAsync(id);
+        var todoItem = await _context.TodoItems.FindAsync(id);
 
-        if (item == null)
+        if (todoItem == null)
         {
             return NotFound();
         }
 
-        return item;
+        return ItemToDTO(todoItem);
     }
 
     [HttpPost]
-    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
     {
-        _context.TodoItems.Add(item);
+        var todoItem = new TodoItem
+        {
+            IsComplete = todoDTO.IsComplete,
+            Name = todoDTO.Name
+        };
+
+        _context.TodoItems.Add(todoItem);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, ItemToDTO(todoItem));
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+    public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
     {
-        if (id != item.Id)
+        if (id != todoDTO.Id)
         {
             return BadRequest();
         }
 
-        _context.Entry(item).State = EntityState.Modified;
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
 
-        await _context.SaveChangesAsync();
+        todoItem.Name = todoDTO.Name;
+        todoItem.IsComplete = todoDTO.IsComplete;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+        {
+
+            return NotFound();
+        }
 
         return NoContent();
     }
@@ -71,4 +92,16 @@ public class TodoItemsController : ControllerBase
 
         return NoContent();
     }
+
+    private bool TodoItemExists(long id)
+    {
+        return _context.TodoItems.Any(e => e.Id == id);
+    }
+
+    private static TodoItemDTO ItemToDTO(TodoItem todoItem) => new TodoItemDTO
+    {
+        Id = todoItem.Id,
+        Name = todoItem.Name,
+        IsComplete = todoItem.IsComplete
+    };
 }
