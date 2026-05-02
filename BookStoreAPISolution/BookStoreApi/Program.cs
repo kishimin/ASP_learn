c26cdf25ead66305b99a1a654281ce46e1df8f2a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-02 20:24:44 +0900

    feat: 構成モデルを追加する

--- BookStoreAPISolution/BookStoreApi/Program.cs
+++ BookStoreAPISolution/BookStoreApi/Program.cs
@@ -1,6 +1,9 @@
+using BookStoreApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
