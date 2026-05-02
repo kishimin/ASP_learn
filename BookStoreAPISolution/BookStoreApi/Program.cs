637223133b2f1b911d441b25f624053b5b9c61bf
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-02 21:12:49 +0900

    feat: CRUD操作のサービスを追加する

--- BookStoreAPISolution/BookStoreApi/Program.cs
+++ BookStoreAPISolution/BookStoreApi/Program.cs
@@ -1,9 +1,12 @@
 using BookStoreApi.Models;
+using BookStoreApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
