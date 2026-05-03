c5b9c7d167871e4b99393635e7ba11df4d7f1d0c
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-03 13:10:46 +0900

    feat:Entity Framework

--- FinShark/api/Data/ApplicationDbContext.cs
+++ FinShark/api/Data/ApplicationDbContext.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
 
 namespace api.Data
 {
--- FinShark/api/Program.cs
+++ FinShark/api/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddContext<ApplicationDBContext>(options =>
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
