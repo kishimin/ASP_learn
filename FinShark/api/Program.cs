1e7b5a952fce98abad135b34dcaf22b377aa82d4
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-05-03 12:22:30 +0900

    fix: appsettings.Development.jsonをgit管理しないように変更

--- FinShark/api/Program.cs
+++ FinShark/api/Program.cs
@@ -4,6 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
